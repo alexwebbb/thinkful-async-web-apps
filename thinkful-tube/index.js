6265027f4cae2464c045f2ced01c3c27f158a64e
Alex Webb <alexwebbb@yahoo.com>
2017-11-20 20:48:00 +0000

    about to refactor to revealing module pattern

--- thinkful-tube/index.js
+++ thinkful-tube/index.js
@@ -5,10 +5,10 @@ const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 const currentState = {
     queryString: '',
     queryResult: {}
-}
+};
 
 function updateString( qString ) {
-    currentState.queryResult = qString
+    currentState.queryResult = qString;
 }
 
 function updateResult( qResult ) {
@@ -21,9 +21,10 @@ function getDataFromApi( searchTerm, callback, pageToken = null ) {
         part: 'snippet',
         q: `${searchTerm}`,
         maxResults: 6,
+        type: 'video',
         nextPageToken: pageToken,
         key: 'AIzaSyCEO_Lc9GBl5vjSoGYq-BpXE3VS3x-XhIc'
-    }
+    };
     $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
