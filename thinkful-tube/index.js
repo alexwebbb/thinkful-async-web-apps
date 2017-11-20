0a41b01e30672b556624a71846c912c1ba0be201
Alex Webb <alexwebbb@yahoo.com>
2017-11-20 22:31:26 +0000

    module pattern implemented

--- thinkful-tube/index.js
+++ thinkful-tube/index.js
@@ -1,101 +1,108 @@
 /* global $ */
 
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-
-const currentState = {
-    queryString: '',
-    queryResult: {}
-};
-
-function updateString( qString ) {
-    currentState.queryResult = qString;
-}
-
-function updateResult( qResult ) {
-    currentState.queryResult = qResult;
-}
-
-function getDataFromApi( searchTerm, callback, pageToken = null ) {
+var App = (function () {
     
-    const query = {
-        part: 'snippet',
-        q: `${searchTerm}`,
-        maxResults: 6,
-        type: 'video',
-        nextPageToken: pageToken,
-        key: 'AIzaSyCEO_Lc9GBl5vjSoGYq-BpXE3VS3x-XhIc'
+    const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+    
+    const currentState = {
+        queryString: '',
+        queryResult: {}
     };
-    $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
-}
-
-
-function handleInput() {
     
+    const updateString = function( qString ) {
+        currentState.queryResult = qString;
+    }
     
-    $('#js-search-form').submit(function( event ) {
-        event.preventDefault();
+    const updateResult = function( qResult ) {
+        currentState.queryResult = qResult;
+    }
+    
+    const getDataFromApi = function( searchTerm, callback, pageToken = null ) {
         
-        const field = $('#top-search');
+        const query = {
+            part: 'snippet',
+            q: `${searchTerm}`,
+            maxResults: 6,
+            type: 'video',
+            nextPageToken: pageToken,
+            key: 'AIzaSyCEO_Lc9GBl5vjSoGYq-BpXE3VS3x-XhIc'
+        };
+        $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+    }
+    
+    
+    const handleInput = function() {
         
-        updateString(field.val());
         
-        getDataFromApi(field.val(), renderResult);
-    });
-}
-
-function handleClick() {
+        $('#js-search-form').submit(function( event ) {
+            event.preventDefault();
+            
+            const field = $('#top-search');
+            
+            updateString(field.val());
+            
+            getDataFromApi(field.val(), renderResult);
+        });
+    }
     
-    
-    $('.js-figure').click(function( event ) {
+    const handleClick = function() {
         
-        console.log($(this).attr('data-item'));
-    });
-    
-}
-
-
-function renderResult( result ) {
-    console.log(result);
-    
-    updateResult(result);
-    
-    $('#js-main').html(
-        result.items.reduce(
-            (s, v) => s + returnArticle(v),
-            '')
-        );
         
-    handleClick();
-}
+        $('.js-figure').click(function( event ) {
+            
+            console.log($(this).attr('data-item'));
+        });
+        
+    }
+    
+    
+    const renderResult = function( result ) {
+        console.log(result);
+        
+        updateResult(result);
+        
+        $('#js-main').html(
+            result.items.reduce(
+                (s, v) => s + returnArticle(v),
+                '')
+            );
+            
+        handleClick();
+    }
+    
+    const returnArticle = function( item ) {
+        
+        const s = item.snippet;
+        
+        return `
+            <article>
+    			<figure class="js-figure" data-item="${item.id.videoId}">
+    				<img 
+    					src="${s.thumbnails.medium.url}" 
+    					alt="${s.description}"
+    					>
+    				<figcaption>
+    					<h1>
+    					    ${s.title}
+    					</h1>
+    				</figcaption>
+    			</figure>
+    		</article>
+        `;
+    }
+  
+  return {
+      handleInput: handleInput
+  };
 
-function returnArticle( item ) {
-    
-    const s = item.snippet;
-    
-    return `
-        <article>
-			<figure class="js-figure" data-item="${item.id.videoId}">
-				<img 
-					src="${s.thumbnails.medium.url}" 
-					alt="${s.description}"
-					>
-				<figcaption>
-					<h1>
-					    ${s.title}
-					</h1>
-				</figcaption>
-			</figure>
-		</article>
-    `;
-}
+})();
+
+
 
 
 function main () {
     
-    handleInput();
-    handleClick();
-    
-    
+    App.handleInput();
     
     return 0;
 }
