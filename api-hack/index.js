cb2edfe07c6193079deceab41ccc5c0024a65783
Alex Webb <alexwebbb@protonmail.com>
2017-11-28 06:04:41 -0600

    basic layout setup... just need to get a better understanding of enter / update wrt d3

--- api-hack/index.js
+++ api-hack/index.js
@@ -2,9 +2,41 @@
 // When the user changes the bounds of the rectangle,
 // an info window pops up displaying the new bounds.
 
+
+///// Google Maps
+
 var rectangle;
 var map;
 var infoWindow;
+
+
+///// D3
+
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
+
+// Set the ranges
+var x = d3.scaleLinear().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// Define the line
+var valueline = d3.line()
+    .x(function(d, i) { return x(i); })
+    .y(function(d) { return y(d.elevation); });
+    
+// Adds the svg canvas
+var svg = d3.select("#graph-container")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -51,3 +83,28 @@ function showNewRect(event) {
 
     infoWindow.open(map);
 }
+
+
+// Get the data
+d3.json("data2.json", function(error, data) {
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d, i) { return i; }));
+    y.domain([-500, d3.max(data, function(d) { return d.elevation; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", valueline);
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the Y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+});
