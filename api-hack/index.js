592c547cb5c16626935d1bc85a4616296339d254
Alex Webb <alexwebbb@protonmail.com>
2017-11-29 02:22:54 -0600

    HA!!! It works!!!

--- api-hack/index.js
+++ api-hack/index.js
@@ -3,37 +3,42 @@
 // an info window pops up displaying the new bounds.
 
 
+///// Data Object
+
+let dataObject = [];
+
 ///// Google Maps
 
-var rectangle;
-var map;
-var infoWindow;
+let rectangle;
+let map;
+let elevator;
+let infoWindow;
 
 
 ///// D3
 
 // Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
+let margin = { top: 30, right: 20, bottom: 30, left: 50 },
     width = 600 - margin.left - margin.right,
     height = 270 - margin.top - margin.bottom;
 
 // Set the ranges
-var x = d3.scaleLinear().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+let x = d3.scaleLinear().range([0, width]);
+let y = d3.scaleLinear().range([height, 0]);
 
 // Define the line
-var valueline = d3.line()
+let valueline = d3.line()
     .x(function(d, i) { return x(i); })
     .y(function(d) { return y(d.elevation); });
-    
+
 // Adds the svg canvas
-var svg = d3.select("#graph-container")
+let svg = d3.select("#graph-container")
     .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
 
 ////// GOOGLE MAPS FUNCTIONS
@@ -43,8 +48,10 @@ function initMap() {
         center: { lat: 44.5452, lng: -78.5389 },
         zoom: 9
     });
+    elevator = new google.maps.ElevationService;
 
-    var bounds = {
+
+    let bounds = {
         north: 44.599,
         south: 44.490,
         east: -78.443,
@@ -70,10 +77,11 @@ function initMap() {
 
 /** @this {google.maps.Rectangle} */
 function showNewRect(event) {
-    var ne = rectangle.getBounds().getNorthEast();
-    var sw = rectangle.getBounds().getSouthWest();
+    let ne = rectangle.getBounds().getNorthEast();
+    let sw = rectangle.getBounds().getSouthWest();
+    console.log(rectangle.getBounds());
 
-    var contentString = '<b>Rectangle moved.</b><br>' +
+    let contentString = '<b>Rectangle moved.</b><br>' +
         'New north-east corner: ' + ne.lat() + ', ' + ne.lng() + '<br>' +
         'New south-west corner: ' + sw.lat() + ', ' + sw.lng();
 
@@ -82,54 +90,81 @@ function showNewRect(event) {
     infoWindow.setPosition(ne);
 
     infoWindow.open(map);
+
+    // construct path request
+    let path = [
+        { lat: ne.lat(), lng: sw.lng() },
+        { lat: sw.lat(), lng: ne.lng() }
+    ];
+
+    // initiate path request
+    elevator.getElevationAlongPath({
+        'path': path,
+        'samples': 30
+    }, updateData);
+
+
 }
 
 
 // Get the data
-d3.json("data2.json", function(error, data) {
+d3.json("data2.json", function(error, dataReturn) {
 
-    
+    dataObject = dataReturn;
+    console.log(dataObject);
+    initGraph(dataObject);
 
 });
 
-function initGraph() {
-	// Scale the range of the data
+
+function updateData(data, status) {
+	dataObject = data;
+	updateGraph(data);
+}
+
+function initGraph(data) {
+    // Scale the range of the data
     x.domain(d3.extent(data, function(d, i) { return i; }));
-    y.domain([-500, d3.max(data, function(d) { return d.elevation; })]);
+    y.domain([-100, d3.max(data, function(d) { return d.elevation; })]);
 
     // Add the valueline path.
     svg.append("path")
         .data([data])
         .attr("class", "line")
-        .attr("d", valueline);
+        .attr("d", valueline(data));
 
     // Add the X Axis
     svg.append("g")
+        .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
     // Add the Y Axis
     svg.append("g")
+        .attr("class", "y axis")
         .call(d3.axisLeft(y));
 }
 
-function updateGraph() {
-	// Scale the range of the data
-    x.domain(d3.extent(data, function(d, i) { return i; }));
-    y.domain([-500, d3.max(data, function(d) { return d.elevation; })]);
 
+function updateGraph(data) {
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d, i) { return i; }));
+    y.domain([-100, d3.max(data, function(d) { return d.elevation; })]);
+
+
+    let svg = d3.select("#graph-container").transition();
     // Add the valueline path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
+    svg.select(".line")
+        .duration(750)
+        .attr("d", valueline(data));
 
     // Add the X Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
+    svg.select(".x.axis")
+        .duration(750)
         .call(d3.axisBottom(x));
 
     // Add the Y Axis
-    svg.append("g")
+    svg.select(".y.axis")
+        .duration(750)
         .call(d3.axisLeft(y));
 }
